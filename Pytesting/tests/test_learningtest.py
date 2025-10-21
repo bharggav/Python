3e1f270d1172895b5ea37bc0c9a1eced1a6e4e02
bharggav <bharggavgalla@gmail.com>
2025-10-21 19:33:55 +0530

    releasing new version

---
+++ Pytesting/tests/test_learningtest.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('C:/Users/bharg/Documents/GitHub/Python/Pytest') # Add the parent directory to sys.path  
+#import pytest
+import time
+import learningtest as test1
+print(time.time())
+
