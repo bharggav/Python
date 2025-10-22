bd710a39c299b5c1612ea4d47f3672403eec7829
bharggav <bharggavgalla@gmail.com>
2025-10-22 10:17:51 +0530

    new push

--- Pytesting/tests/test_learningtest.py
+++ Pytesting/tests/test_learningtest.py
@@ -6,3 +6,12 @@ print(time.time())
 def testadd():
     assert test1.add(1,2) == 3
 
+def testsub():
+    assert test1.sub(2,1) == 1
+def testmul():
+    assert test1.mul(2,3) == 6
+def testdiv():
+    assert test1.div(6,2) == 3 # Fixed 'dev' to 'div'
+def testdiv_zero():
+    with pytest.raises(ValueError):
+        test1.div(1,0)
